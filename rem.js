aa88ee1991756d22c01686af0799e27f7a262938
SanF <464884785@qq.com>
2019-10-25 17:59:35 +0800

    Updates

---
+++ rem.js
@@ -0,0 +1,19 @@
+(function(doc, win) {
+    var docEl = doc.documentElement;
+    var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+    var recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth >= 750) {
+                docEl.style.fontSize = '100px';
+            } else {
+                var fontSize = 50 * (clientWidth / 375);
+                window.xPixelRatio = 100 / fontSize;
+                docEl.style.fontSize = fontSize + 'px';
+            }
+        };
+    recalc();
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
